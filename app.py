135022c9ced2703cd939aeb04423c9e4d11571df
Arthur Khadraoui-Nicod <artoche10@yahoo.FR>
2021-12-01 16:45:48 +0100

    added characters list

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import random
+from Army import Army
 from Character import Character
 import csv
 
 characters_list=[]
 
-with open (r"characters.csv",
+with open (r'characters.csv',
            newline='') as csvfile:
-    character = csv.reader(csvfile,delimiter=';',quotechar='|')
+    character = csv.reader(csvfile,delimiter=',',quotechar='|')
     rows = []
+    header = next(character)
     for row in character:
-        rows.append(row)
+        characters_list.append(Character(row[0],row[1],row[2],row[3],row[4]))
 
+moralValue = random.uniform(20,100)
