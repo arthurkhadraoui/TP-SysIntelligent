2e454c902c1553de581fb98fb8ce1d3d65829e50
Arthur Khadraoui-Nicod <artoche10@yahoo.FR>
2021-12-02 08:49:40 +0100

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,4 @@
+import matplotlib
 
 inputs = [[0,0],[0,1],[1,0],[1,1]]
 outputs = [0,0,0,1]
@@ -8,7 +9,7 @@ comp = []
 
 for w1 in range(-5,6):
     for w2 in range(-5,6):
-        for i in inputs:
-            sol=w1*i[0]+w2*i[1]
-            e = 0.5*(sol-outputs[i])
-            comp.append(sol,e)
+        for i in range(4):
+            sol=w1*inputs[i][0]+w2*inputs[i][1]
+            e = 0.5*(sol-outputs[i])*(sol-outputs[i])
+            comp.append([sol,e])
